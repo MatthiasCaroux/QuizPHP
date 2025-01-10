ec043858b21d0fa913865f3f95fb104bd8cce10b
Niveau Samuel <o22204836@or-iut-i201-f21.iut45.univ-orleans.fr>
2025-01-10 10:22:23 +0100

    enfin ça marche

--- index.php
+++ index.php
@@ -1,40 +1,27 @@
-<?php 
+<?php
 
+require_once('src/Autoloader.php');
+require_once('./questions/RadioQuestions.php');
 
+// Charger le fichier JSON
+$jsonData = json_decode(file_get_contents("https://geoffroycochard.github.io/iuto.but2.php/resources/data/model.json")
+, true);
 
-require_once __DIR__ . '/Classes/AutoLoader.php';
-require_once __DIR__ .'/includes/header.php';
+// Liste des questions
+$questions = array();
 
-
-
-AutoLoader::register();
-
-use models\Questions;
-use models\Questions_simple;
-use models\Provider;
-
-
+foreach ($jsonData as $data) {
+    if ($data['type'] === 'radio') {
+        $questions[] = new RadioQuestions($data['uuid'], $data['label'], $data['choices'], $data['correct']);
+    }
+    // } elseif ($data['type'] === 'text') {
+    //     $questions[] = new TextField($data['uuid'], $data['label']);
+    // }
+}
 ?>
+<h1>Répondez aux questions</h1>
 
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <title>Document</title>
-</head>
-<body>
-    <main>
-        <h1>Questions : </h1>
-        <?php
-            $provider = new Provider();        
-            $questions = $provider->getQuestions();
-            foreach ($questions as $question) {
-                echo $question->render();
-            }
-        ?>
-    </main>
-</body>
-</html>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <?php foreach ($questions as $question): ?>
+        <?php $question->display(); ?>
+    <?php endforeach; ?>
